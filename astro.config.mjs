bb47f31131cb9571530232b674448ac45ea03516
Takashi Takizawa <taki@cyber.email.ne.jp>
2024-02-19 08:58:14 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,26 +9,32 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: "文字コード",
+					collapsed: true,
 					autogenerate: { directory: "charcode" },
 				},
 				{
 					label: "Linux/FLOSS",
+					collapsed: true,
 					autogenerate: { directory: "floss" },
 				},
 				{
 					label: "DNS",
+					collapsed: true,
 					autogenerate: { directory: "dns" },
 				},
 				{
 					label: "MAIL",
+					collapsed: true,
 					autogenerate: { directory: "mail" },
 				},
 				{
 					label: "Mutt",
+					collapsed: true,
 					autogenerate: { directory: "mutt" },
 				},
 				{
 					label: "SpamAssassin",
+					collapsed: true,
 					autogenerate: { directory: "spamassassin" },
 				},
 				{
